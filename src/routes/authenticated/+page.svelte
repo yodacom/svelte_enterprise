c5809a5dd49d6dd7848a941d3e5b217e95291663
Jeremy Black <jb@yodacom.com>
2023-05-09 11:49:24 -0600

    Initial commit

---
+++ src/routes/authenticated/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+
+	$: user = $page.data.user
+</script>
+
+<div class="p-4">
+	<h1>Profile</h1>
+	<p class="mt-4 capitalize">Welcome, {user?.username}!</p>
+</div>
